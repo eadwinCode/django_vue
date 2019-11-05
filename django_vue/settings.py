5a7977dfce03b43ba9363f268fdd21e30e355977
Eadwin <ezeudoh.tochukwu@gmail.com>
2019-11-05 08:18:26 +0100

    Testing private static files

--- django_vue/settings.py
+++ django_vue/settings.py
@@ -107,9 +107,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend'),
-)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
